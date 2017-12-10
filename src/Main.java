bccfbbaa779cf1d9898a12259f2c436aaa225757
dobosizsolt <dobosi.zsolt@gmail.com>
2017-12-10 16:55:34 +0100

    #3

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,12 @@
+import hu.helixlab.Game;
+
 public class Main {
 
     public static void main(String[] args) {
 
         System.out.println(PasswordGenerator.generatePassword(8));
+
+       // Game game = new Game();
+        //Game.playBarchobaWithNumbers();
     }
 }
