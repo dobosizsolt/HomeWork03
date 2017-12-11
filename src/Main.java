4007c019a5bbdfadffc2ab5d380c8e2b2fc4fff8
dobosizsolt <dobosi.zsolt@gmail.com>
2017-12-11 20:49:01 +0100

    #3

--- src/Main.java
+++ src/Main.java
@@ -3,16 +3,15 @@ public class Main {
 
     public static void main(String[] args) {
 
-        System.out.println(PasswordGenerator.generatePassword(8));
+        //System.out.println(PasswordGenerator.generatePassword(8));
 
-       // Game game = new Game();
-        //Game.playBarchobaWithNumbers();
+        Gamev2.playBarchobaWithNumbers();
 
-        Temperature temperature = new Temperature();
-        Temperature.getTemperatureDataFromConsole();
+        //Temperature temperature = new Temperature();
+        //Temperature.getTemperatureDataFromConsole();
 
-        System.out.println(Temperature.printHighestTemperature());
-        System.out.println(Temperature.printLowestTemperature());
+        //System.out.println(Temperature.printHighestTemperature());
+        //System.out.println(Temperature.printLowestTemperature());
 
     }
 }
